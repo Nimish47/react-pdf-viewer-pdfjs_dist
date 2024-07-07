001ac35e4c82794a92f1d454bc651f393ec39906
Nimish47 <nimishnishant1995@gmail.com>
2024-07-07 18:43:06 +0530

    View pdf and others

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import styles from './App.module.css'
+import Home from './component/Home';
 import PDFContainer from "./component/PDFContainer";
 
 function App() {
+
+  const [toggle, setToggle] = useState(false)
+  const toggleFunc = () => { setToggle(!toggle) }
+
   return (
-    <div className={styles.appContainer}>
-      <PDFContainer />
+    <div className={styles.container}>
+      <div
+        className={styles.home}
+        onClick={toggleFunc}
+      >
+        <Home />
+      </div>
+      {toggle && (<div className={styles.pdfContainer}>
+        <PDFContainer />
+      </div>)
+      }
     </div>
   );
 }
---
+++ src/component/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+
+function Home() {
+    return (
+        <>
+           View PDF
+        </>
+    );
+}
+
+export default Home;
